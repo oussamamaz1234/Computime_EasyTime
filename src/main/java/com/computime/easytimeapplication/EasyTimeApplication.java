e37d954eb8a38ca5bc59d87df1962806aeac08e4
Oussama <oussamamazouz81@gmail.com>
2024-07-20 23:41:32 +0100

    Commit pour la forme du proget contient l'organisation comme service seulement

---
+++ src/main/java/com/computime/easytimeapplication/EasyTimeApplication.java
@@ -0,0 +1,13 @@
+package com.computime.easytimeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/computime/easytimeapplication/EasyTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computime.easytimeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
