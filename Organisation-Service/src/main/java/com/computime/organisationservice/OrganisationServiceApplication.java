fa515f0b6cf5800e32e67bdf30b7f05c05d50975
Oussama <oussamamazouz81@gmail.com>
2024-07-21 01:11:16 +0100

    Voci ou on va travailler dans Organization service

--- Organisation-Service/src/main/java/com/computime/organisationservice/OrganisationServiceApplication.java
+++ Organisation-Service/src/main/java/com/computime/organisationservice/OrganisationServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrganisationServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hahao");
+
 		SpringApplication.run(OrganisationServiceApplication.class, args);
 	}
 
